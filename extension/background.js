40344f0c006e9faa4815f995f2776664fbe3dac4
Guilherme Goncalves <guilherme.p.gonc@gmail.com>
2017-10-29 16:17:14 +0000

    Add proof-of-concept browser extension.

---
+++ extension/background.js
@@ -0,0 +1,85 @@
+const URL_REGEXS = [
+    /abcnews.go.com\/.+/i,
+    /arstechnica.com\/.+/i,
+    /bbc.co.uk\/.+/i,
+    /bbc.com\/.+/i,
+    /business-standard.com\/.+/i,
+    /cnn.com\/.+/i,
+    /economist.com\/.+/i,
+    /guardian.co.uk\/.+/i,
+    /theguardian.com\/.+/i,
+    /hollywoodreporter.com\/.+/i,
+    /huffingtonpost.com\/.+/i,
+    /irishtimes.com\/.+/i,
+    /independent.co.uk\/.+/i,
+    /npr.org\/.+/i,
+    /nytimes.com\/.+/i,
+    /politico.com\/.+/i,
+    /rollingstone.com\/.+/i,
+    /spiegel.de\/.+/i,
+    /time.com\/.+/i,
+    /variety.com\/.+/i,
+    /washingtonpost.com\/.+/i,
+    /wired.com\/.+/i,
+]
+
+const SIMILARITY_URL = 'http://localhost:5000/search';
+
+function getDocumentContents(callback) {
+  let script = 'document.documentElement.outerHTML';
+  chrome.tabs.executeScript({
+    code: script
+  }, (result) => { callback(result); });
+}
+
+function fetchSimilarArticles(html, url, callback) {
+  let formData = new FormData();
+  formData.append('html', html);
+  formData.append('url', url);
+  let o = {method: 'POST', mode: 'cors', body: formData};
+  window.fetch(SIMILARITY_URL, o).then(
+    (response) => { return response.json(); }).then(callback);
+}
+
+function refreshSuggestionsForTab(tab, callback) {
+  if (!tab.url) return;
+  let match = false;
+  for (let i = 0; i < URL_REGEXS.length; i++) {
+    if (tab.url.match(URL_REGEXS[i])) {
+      match = true;
+    }
+  }
+  if (!match) return;
+  getDocumentContents((html) => {
+    fetchSimilarArticles(html, tab.url, callback);
+  });
+}
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.get(activeInfo.tabId, (tab) => {
+    refreshSuggestionsForTab(tab, (articles) => {
+      if (articles.length) {
+        chrome.pageAction.show(tab.id);
+      }
+    });
+  });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status == 'complete') {
+    refreshSuggestionsForTab(tab, (articles) => {
+      if (articles.length) {
+        chrome.pageAction.show(tab.id);
+      }
+    });
+  }
+  chrome.pageAction.hide(tab.id);
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    // TODO this should reuse the response we got before?
+    refreshSuggestionsForTab(tabs[0], sendResponse);
+  });
+  return true;
+});
---
+++ extension/popup.js
@@ -0,0 +1,20 @@
+function renderPopUp(articles) {
+  let container = document.getElementById("container");
+  container.innerHTML = '<ol></ol>';
+  for (let i = 0; i < articles.length; i++) {
+    let li = document.createElement('li');
+    let a = document.createElement('a');
+    a.href = articles[i].url;
+    a.target = '_blank';
+    a.textContent = articles[i].title;
+    li.appendChild(a);
+    li.appendChild(document.createTextNode(
+      ' (' + articles[i].similarity + ')'));
+    container.firstChild.appendChild(li);
+  }
+}
+
+chrome.runtime.sendMessage({}, (articles) => {
+  // https://bugs.chromium.org/p/chromium/issues/detail?id=428044
+  setTimeout(() => { renderPopUp(articles); }, 100);
+});
